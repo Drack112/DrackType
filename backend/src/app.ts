38192ae5b22c25ef1894385254ddcdf312325b7f
Drack112 <onepessoa3@gmail.com>
2025-10-16 00:09:08 -0300

    feat: basic setup o the express server

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,24 @@
-import express, { urlencoded, json } from "express";
+import express, { json, urlencoded } from "express";
 import cors from "cors";
 import helmet from "helmet";
+
+import { createETagGenerator } from "./utils/etag";
+import { COMPATIBILITY_CHECK_HEADER } from "@dracktype/contracts";
+import { compabilityCheckMiddleware } from "./middleware/compabilityCheck";
+
+const etag = createETagGenerator({ weak: true });
 
 function buildApp(): express.Application {
   const app = express();
 
   app.use(urlencoded({ extended: true }));
   app.use(json());
+  app.use(cors({ exposedHeaders: [COMPATIBILITY_CHECK_HEADER] }));
   app.use(helmet());
 
   app.set("trust proxy", 1);
+
+  app.use(compabilityCheckMiddleware);
 
   return app;
 }
